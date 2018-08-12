0e96528c95b6b3e73044aba0ea9d99b7c4fbf163
twined quartz <fmorisato@gmail.com>
2018-08-12 17:21:11 +0900

    documentation of  TopMenu

--- src/js/top-menu.js
+++ src/js/top-menu.js
@@ -1,4 +1,34 @@
 //import $ from "../../../jslib/jquery-2.2.0.min.js"
+/**
+ * top menu extensionの作り方
+ * 
+ * 次の4つを必ずexportする
+ * 
+ * 1. {function} {String} template(uiState)
+ *  メニューウィンドウの内容を表すHTMLを返す.
+ *  これはメニュー登録時にdiv要素にラップされる.
+ * 2. {function} {void} eventSetter(emitter, uiState)
+ *  上記のHTML要素にイベントハンドラを与えるメソッド.
+ *  emitterに登録されたactionを用いることができる.
+ * 3. {Object} option = {
+ *  {String} label : "Label of Button shown in top menu"
+ *  {Bool} draggable : Either draggable or not
+ * }
+ *  トップメニューに表示されるメニュー表示ボタンの名前と, 
+ *  ドラッグイベントによる移動の可否を定義する.
+ * 4. {String} style = "css string defining geometry of menu contents"
+ *  表示されるメニューウィンドウのサイズやpaddingを定義する.
+ *  (templateが返すHTML要素をラップしたdiv要素に与えられるスタイル)
+ * 
+ * Event Emitterに追加する関数がある場合次を定義する
+ * 5. {Array} exportToEmitter = [
+ *  {
+ *    {String} type : "action type of Emitter",
+ *    {function} action : {void} method
+ *  }
+ * ]
+ */
+
 export default class TopMenu {
   constructor(menuBarId, overlayId, eventEmitter, uiState) {
     this.overlayId = overlayId;
