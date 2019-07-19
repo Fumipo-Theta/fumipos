095dec83709afecf115a8d07af07db39bea387dd
twined quartz <fmorisato@gmail.com>
2019-07-19 15:49:59 +0900

    modify icon urls

--- src/js/graph-btn-refresh.js
+++ src/js/graph-btn-refresh.js
@@ -2,14 +2,14 @@ export const btnName = "nav_refresh";
 
 export const style = `
 a.${btnName}::before{
-  content:url(../image/sync-alt-solid.svg);
+  content:url(./image/sync-alt-solid.svg);
   position: relative;
 }
 
 a.${btnName}:hover::before,
 a.${btnName}:focus::before{
   transition: all .3s;
-  content:url(../image/sync-alt-solid_hover.svg);
+  content:url(./image/sync-alt-solid_hover.svg);
 }
 `
 
